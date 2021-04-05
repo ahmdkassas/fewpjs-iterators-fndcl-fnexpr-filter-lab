515b634190308be8aef675173bb74dba45f06d8c
ahmed <ahm.gmal@gmail.com>
2021-04-05 10:36:41 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+
+
+function findMatching([...Arry], name){
+    return Arry.filter(x => x.toLowerCase() == name.toLowerCase());
+  }
+
+
+function fuzzyMatch([...Arry], name){
+    return Arry.filter(x => x.startsWith(name));
+  }
+
+function matchName([...Arry], name){
+    return Arry.filter(x => x.name === name);
+}
